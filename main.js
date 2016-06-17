0c11b322f63842832cc4eb4e180053f226be9340
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-17 14:13:10 -0400

    Added data to index.html via hardcode + js, added some css styling

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ var total = prices.reduce(function(prev, curr) {
 
 var average = (total / items.length)
 
-var avg = average.toFixed(2)
+var avg = ("The average price of all items is $" + average.toFixed(2))
 
+var answer1 = document.querySelector("#answer1");
+answer1.innerHTML = avg
 
 //--------------2---------------------------
 //Show me how to get an array of items that cost between $14.00 and $18.00 USD
@@ -27,6 +29,8 @@ var betweenArray = between.map(function(item) {
   return item.title;
 });
 
+var answer2 = document.querySelector("#answer2");
+answer2.innerHTML = betweenArray
 //--------------3----------------------------
 //Which item has a "GBP" currency code? Display it's name and price.
 var currency = items.filter(function(item) {
@@ -36,9 +40,11 @@ var currency = items.filter(function(item) {
 });
 
 var currencyCode = currency.map(function(item) {
-  return (item.title + " costs " + item.price)
+  return (item.title + " costs £" + item.price)
 });
 
+var answer3 = document.querySelector("#answer3");
+answer3.innerHTML = currencyCode
 //---------------4-------------------------------
 //Display a list of all items who are made of wood.
 
@@ -54,6 +60,8 @@ var itemsWithWood = wood.map(function(item) {
 });
 
 
+var answer4 = document.querySelector("#answer4");
+answer4.innerHTML = itemsWithWood
 //-------------5-----------------------------------
 //Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
 
@@ -74,6 +82,8 @@ var materialList = manyMaterial.map(function(item) {
   // matsArray
 });
 
+var answer5 = document.querySelector("#answer5");
+answer5.innerHTML = manyMaterialItem + materialList
 //---------------6-----------------------------------
 //How many items were made by their sellers?
 
@@ -85,3 +95,5 @@ var whoMade = items.filter(function(item) {
 });
 
 var made = (whoMade.length + " were made by their sellers!")
+var answer6 = document.querySelector("#answer6");
+// answer6.innerHTML =
