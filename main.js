2c30abc34196bd08b08f3dc5fda4e3545dad7c74
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-28 16:18:33 -0400

    Fixed assignment

--- main.js
+++ main.js
@@ -17,26 +17,27 @@ answer1.innerHTML = avg
 //--------------2---------------------------
 //Show me how to get an array of items that cost between $14.00 and $18.00 USD
 var between = items.filter(function(item) {
-  if (item.price > 14 && item.price < 18) {
-    return item;
-  }
+  return item.price > 14 && item.price < 18
 });
 
 var betweenArray = between.map(function(item) {
   return item.title;
 });
 
-var answer2 = document.querySelector("#answer2");
-answer2.innerHTML = betweenArray
+// console.log(betweenArray)
+betweenArray.forEach(function (title) {
+  document.getElementById("answer2").innerHTML += title + "<br />";
+});
+
+// var answer2 = document.querySelector("#answer2");
+// answer2.innerHTML = betweenArray
 
 
 
 //--------------3----------------------------
 //Which item has a "GBP" currency code? Display it's name and price.
 var currency = items.filter(function(item) {
-  if (item.currency_code == "GBP") {
-    return item;
-  }
+  return item.currency_code == "GBP"
 });
 
 var currencyCode = currency.map(function(item) {
@@ -52,55 +53,58 @@ answer3.innerHTML = currencyCode
 //Display a list of all items who are made of wood.
 
 var wood = items.filter(function(item) {
-  var material = item.materials
-  if (material.includes("wood")) {
-    return item;
-  }
+  return item.materials.includes("wood")
 });
 
 var itemsWithWood = wood.map(function(item) {
-  return (item.title + " is made of wood!");
+  return ('* ' + item.title + " is made of wood!");
 });
 
-
-var answer4 = document.querySelector("#answer4");
-answer4.innerHTML = "<p>" + itemsWithWood + "</p>"
-
+itemsWithWood.forEach(function (title) {
+  document.getElementById("answer4").innerHTML += title + "<br />";
+});
 
 
 //-------------5-----------------------------------
 //Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
 
 var manyMaterial = items.filter(function(item) {
-  var material = item.materials
-  if (material.length > 8) {
-    return item;
-  }
+  return item.materials.length >= 8
 });
 
 var manyMaterialItem = manyMaterial.map(function(item) {
   return (item.title + " is made of " + item.materials.length + " materials!");
 });
 
+var features = manyMaterial.map(function(item) {
+  return [item.title, item.materials.length, item.materials];
+});
+
+
+
 var materialList = manyMaterial.map(function(item) {
   // var matsArray = item.materials;
   return item.materials;
   // matsArray
 });
-
+newHTML = ""
 var answer5 = document.querySelector("#answer5");
-answer5.innerHTML = "<p>" + manyMaterialItem + "</p>"
-answer5.innerHTML += "<p>" + materialList + "</p>"
+manyMaterial.forEach(function(item) {
+  newHTML += "<p>" + item.title + " used" + item.materials.length + " materials" + "</p>";
+  newHTML += "<ul class=\"materials\">";
+  item.materials.forEach(function(material) {
+    newHTML += "<li class=\"material\">" + material + "</li>";
+  });
+  newHTML += "</ul>"
+});
+answer5.innerHTML = newHTML
 
 
 //---------------6-----------------------------------
 //How many items were made by their sellers?
 
 var whoMade = items.filter(function(item) {
-  var seller = item.who_made
-  if (seller == "i_did") {
-    return item;
-  }
+  return item.who_made === "i_did"
 });
 
 var made = (whoMade.length + " were made by their sellers!")
